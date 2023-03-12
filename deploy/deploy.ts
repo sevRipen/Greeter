f842c969685ef6d5e15a7c1ec895fd76420941b2
CryptoRelax <98783023+sevRipen@users.noreply.github.com>
2023-03-12 19:34:14 +0300

    Update deploy.ts

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -8,7 +8,7 @@ export default async function (hre: HardhatRuntimeEnvironment) {
   console.log(`Running deploy script for the Greeter contract`);
 
   // Initialize the wallet.
-  const wallet = new Wallet("<WALLET-PRIVATE-KEY>");
+  const wallet = new Wallet("Здесь впишите свой приватный ключ от мм, ковычки оставить");
 
   // Create deployer object and load the artifact of the contract you want to deploy.
   const deployer = new Deployer(hre, wallet);
